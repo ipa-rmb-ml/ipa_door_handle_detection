2dababcecfbc852f02c143e62745b5d99b0455be
ipa-rmb-ml <44199646+ipa-rmb-ml@users.noreply.github.com>
2019-06-11 15:48:40 +0200

    Add files via upload

--- ros/include/MachineLearning/ipa_train_classifier.h
+++ ros/include/MachineLearning/ipa_train_classifier.h
@@ -14,8 +14,21 @@
 #include <opencv2/ml/ml.hpp>
 
 
-const std::string posDepthImagePath;
-const std::string negDepthImagePath;
+std::string posDepthImagePathTrain;
+std::string negDepthImagePathTrain;
+
+std::string posDepthImagePathTest;
+std::string negDepthImagePathTest;
+
+std::string classifier_path;
+
+
+struct DataStruct
+{
+    cv::Mat labels;
+    cv::Mat features;
+};
+
 
 
 class TrainClassifier
@@ -32,9 +45,16 @@ std::vector<cv::Mat> readDepthImages(const std::string dir_name);
 
 void generateTrainingData(std::vector<cv::Mat> image_seq, bool label);
 
+static void trainAllClassifier(cv::Mat traing_data,cv::Mat labels,bool trainSVM, bool trainRF,bool trainAB);
+
+static DataStruct buildTrainingData();
+
+static DataStruct buildTestingData();
+
+static void calculatingPredictions(cv::Mat training_data, cv::Mat label_data,CvSVM &SVM);
+
 cv::Mat getLabelData(void);
 cv::Mat getTrainingData(void);
-
 
 cv::Mat label_data_;
 cv::Mat training_data_;
--- ros/src/MachineLearning/ipa_train_classifier.cpp
+++ ros/src/MachineLearning/ipa_train_classifier.cpp
@@ -2,6 +2,14 @@
 
 TrainClassifier::TrainClassifier()
 {
+
+    posDepthImagePathTrain= "/home/rmb-ml/Desktop/TemplateDataBase/DepthImages/Train/Positive/";
+    negDepthImagePathTrain= "/home/rmb-ml/Desktop/TemplateDataBase/DepthImages/Train/Negative/";
+
+    posDepthImagePathTest= "/home/rmb-ml/Desktop/TemplateDataBase/DepthImages/Test/Positive/";
+    negDepthImagePathTest= "/home/rmb-ml/Desktop/TemplateDataBase/DepthImages/Test/Negative/";
+
+    classifier_path= "/home/rmb-ml/Desktop/TemplateDataBase/classifierModel/";
 }
 
 //implementation of LBP
@@ -117,98 +125,261 @@ cv::Mat TrainClassifier::getLabelData(void)
 }
 
 
+void TrainClassifier::calculatingPredictions(cv::Mat training_data,cv::Mat label_data, CvSVM &SVM)
+{
+
+            std::cout << "Calculating Predictions ...\n"<<std::endl;
+            unsigned int truePositive = 0;
+            unsigned int trueNegative = 0;
+            unsigned int falsePositive = 0;
+            unsigned int falseNegative = 0;
+
+            for( int num_img = 0; num_img < training_data.rows; num_img++)
+                {
+                    cv::Mat test_sample = training_data.row(num_img);
+                    int predictedLabel = SVM.predict(test_sample);
+                    int trueLabel =  label_data.at<int>(num_img,0);
+
+                    if ( predictedLabel == 1 && trueLabel == 1 ) ++truePositive;
+                    else if ( predictedLabel == 1 && trueLabel == 0 ) ++falsePositive;
+                    else if ( predictedLabel == 0 && trueLabel == 1) ++falseNegative;
+                    else if ( predictedLabel == 0 && trueLabel == 0) ++trueNegative;
+                    else std::cout << "Invalid Classification (make sure you are using binary labels)" << std::endl; //invali
+                }
+
+                double accuracy = (truePositive + trueNegative) * 100.0 / (truePositive + falsePositive + trueNegative + falseNegative);
+                double precision = truePositive * 100.0 / (truePositive + falsePositive);
+                double true_negative_rate = trueNegative * 100.0 / (trueNegative + falsePositive);
+                double recall = truePositive * 100.0 / (truePositive + falseNegative);
+
+                std::cout
+                    << "-----------------------------------------------------------------------\n"
+                    << "Training Set Classification:\n"
+                    << "-----------------------------------------------------------------------\n"
+                    << "True Positive     : " << truePositive << "\n"
+                    << "False Positive    : " << falsePositive << "\n"
+                    << "True Negative     : " << trueNegative << "\n"
+                    << "False Negative    : " << falseNegative << "\n"
+                    << "Accuracy          : " << accuracy << "\n"
+                    << "Precision         : " << precision << "\n"
+                    << "True negative rate: " << true_negative_rate << "\n"
+                    << "Recall            : " << recall << "\n"
+                    << "-----------------------------------------------------------------------\n";
+            
+
+
+}
+
+
+
+
+void TrainClassifier::trainAllClassifier(cv::Mat training_data,cv::Mat label_data,bool trainSVM, bool trainRF,bool trainAB)
+{
+
+        // ==================================== SVM ================================
+         if(trainSVM)
+         {
+
+
+            std::cout<<"TrainSVM..." << std::endl;
+            CvSVMParams params;
+            params.svm_type    = CvSVM::C_SVC;
+            params.kernel_type = CvSVM::LINEAR;
+            params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+
+            CvSVM SVM;
+            std::string full_path_svm = classifier_path +"SVM_Model.xml";
+            SVM.train(training_data, label_data, cv::Mat(), cv::Mat(), params);
+            
+            TrainClassifier::calculatingPredictions(training_data,label_data,SVM);
+
+            SVM.save(full_path_svm.c_str());
+         }
+
+            // ==================================== SVM ================================
+
+            // ==================================== Boost ================================
+        if(trainAB)
+        {
+
+            std::cout<<"TrainAdaBoost... "<< std::endl;
+            CvBoost boost;
+            
+            boost.train(training_data, CV_ROW_SAMPLE, label_data);
+
+           // TrainClassifier::calculatingPredictions();
+
+            std::string full_path_boost = classifier_path +"Boost_Model.xml";
+            boost.save(full_path_boost.c_str());
+
+        }
+
+        // ==================================== Boost ================================
+
+        // ==================================== Random Trees ================================
+
+        if (trainRF)
+        {
+
+             std::cout<<"TrainRandomForrest..." << std::endl;
+
+            CvRTParams paramsRF;
+            paramsRF.min_sample_count  = 10;
+            paramsRF.max_depth = 5;
+            paramsRF.term_crit = cvTermCriteria( CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 50, 0.1 );
+
+            CvRTrees randomForest;
+        
+            std::string full_path_rf = classifier_path +"RF_Model.xml";
+            randomForest.train(training_data, CV_ROW_SAMPLE, label_data, cv::Mat(), cv::Mat(), cv::Mat(), cv::Mat(), paramsRF );
+
+             //TrainClassifier::calculatingPredictions();
+
+            
+            randomForest.save(full_path_rf.c_str());
+
+          //  float err = randomForest.get_train_error();
+          //  std::cout<<err<<std::endl;
+
+        }
+
+        std::cout<<"===================================================================="<<std::endl;
+
+}
+
+
+DataStruct TrainClassifier::buildTrainingData()
+{
+    TrainClassifier data_pos;
+    TrainClassifier data_neg;
+
+    std::vector<cv::Mat> posImg, negImg;
+
+    //reading files from dir
+    posImg = data_pos.readDepthImages(posDepthImagePathTrain);
+    negImg = data_neg.readDepthImages(negDepthImagePathTrain);
+
+    // get positive and negative training data
+    data_pos.generateTrainingData(posImg,1);
+    data_neg.generateTrainingData(negImg,0);
+
+    cv::Mat label_pos    = data_pos.getLabelData();
+    cv::Mat training_pos = data_pos.getTrainingData();
+
+    cv::Mat label_neg    = data_neg.getLabelData();
+    cv::Mat training_neg = data_neg.getTrainingData();
+
+    // concentrate data
+    cv::Mat training_data;
+    training_data.push_back(training_pos);
+    training_data.push_back(training_neg);
+
+    cv::Mat training_data_labels;
+    training_data_labels.push_back(label_pos);
+    training_data_labels.push_back(label_neg);
+
+    std::cout<<"===============================================" << std::endl;
+    std::cout<<"Training DB"<<std::endl;
+    std::cout<<"Num Pos Samples: " << training_pos.rows <<std::endl;
+    std::cout<<"Num Neg Samples: " << training_neg.rows <<std::endl;
+    std::cout<<"===============================================" << std::endl;
+
+    DataStruct trainingData;
+	trainingData.labels = training_data_labels;
+ 	trainingData.features = training_data;
+
+    return trainingData;
+}
+
+DataStruct TrainClassifier::buildTestingData()
+{
+    TrainClassifier data_pos;
+    TrainClassifier data_neg;
+
+    std::vector<cv::Mat> posImg, negImg;
+
+    //reading files from dir
+    posImg = data_pos.readDepthImages(posDepthImagePathTest);
+    negImg = data_neg.readDepthImages(negDepthImagePathTest);
+
+    // et positive and negative training data
+    data_pos.generateTrainingData(posImg,1);
+    data_neg.generateTrainingData(negImg,0);
+
+    cv::Mat label_pos    = data_pos.getLabelData();
+    cv::Mat testing_pos = data_pos.getTrainingData();
+
+    cv::Mat label_neg    = data_neg.getLabelData();
+    cv::Mat testing_neg = data_neg.getTrainingData();
+
+    // concentrate data
+    cv::Mat testing_data;
+    testing_data.push_back(testing_pos);
+    testing_data.push_back(testing_neg);
+
+    cv::Mat testing_data_labels;
+    testing_data_labels.push_back(label_pos);
+    testing_data_labels.push_back(label_neg);
+
+    std::cout<<"===============================================" << std::endl;
+    std::cout<<"Testing DB"<<std::endl;
+    std::cout<<"Num Pos Samples: " << testing_pos.rows <<std::endl;
+    std::cout<<"Num Neg Samples: " << testing_neg.rows <<std::endl;
+    std::cout<<"===============================================" << std::endl;
+
+    DataStruct testingData;
+	testingData.labels = testing_data_labels;
+ 	testingData.features = testing_data;
+
+}
 
 
 int main(int argc, char **argv)
 {
 
-  std::string posDepthImagePath= "/home/rmb-ml/Desktop/TemplateDataBase/DepthImages/Positive/";
-  std::string negDepthImagePath= "/home/rmb-ml/Desktop/TemplateDataBase/DepthImages/Negative/";
-  std::string classifier_path= "/home/rmb-ml/Desktop/TemplateDataBase/classifierModel/";
+    bool trainSVM =0;
+    bool trainRF = 0;
+    bool trainAB = 0;
 
-   TrainClassifier();
-  
-   
-   TrainClassifier c1;
-   TrainClassifier c2;
+    if (argc < 2)
+    {
+        std::cout<<"You need at least one argument: SVM, RF or AB"<<std::endl;
+    }
 
-   std::vector<cv::Mat> posImg, negImg;
+    else
+    {
 
-    //reading files from dir
-   posImg = c1.readDepthImages(posDepthImagePath);
-   negImg = c2.readDepthImages(negDepthImagePath);
+        for (int i=1; i<argc; i++) 
+        {
+            std::string class_str = argv[i];
 
-   // get positive and negative training data
-   c1.generateTrainingData(posImg,1);
-   c2.generateTrainingData(negImg,0);
+            if(class_str == "SVM")
+            {
+                trainSVM=1;
+            }
+            else if(class_str == "AB")
+            {
+                trainAB =1;
+            }
+            else if(class_str == "RF")
+            {
+                trainRF = 1;
+            }
 
-   cv::Mat label_pos    = c1.getLabelData();
-   cv::Mat training_pos = c1.getTrainingData();
+        }
+     
+    }    
+        TrainClassifier();
 
-   cv::Mat label_neg    = c2.getLabelData();
-   cv::Mat training_neg = c2.getTrainingData();
+        DataStruct trainingData = TrainClassifier::buildTrainingData();
 
-
-   // concentrate data
-    cv::Mat training_data_all;
-    training_data_all.push_back(training_pos);
-    training_data_all.push_back(training_neg);
-
-    cv::Mat label_data_all;
-    label_data_all.push_back(label_pos);
-    label_data_all.push_back(label_neg);
-
-
-    // ==================================== SVM ================================
-
-   CvSVMParams params;
-    params.svm_type    = CvSVM::C_SVC;
-    params.kernel_type = CvSVM::LINEAR;
-    params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
-
-    CvSVM SVM;
-    std::cout<<"Start Training"<<std::endl;
-    std::cout<<"Num Pos Samples: " << training_pos.rows <<std::endl;
-    std::cout<<"Num Neg Samples: " << training_neg.rows <<std::endl;
-     std::cout<<"===============================================" << std::endl;
-
-    std::string full_path_svm = classifier_path +"SVM_Model.xml";
-
-    SVM.train(training_data_all, label_data_all, cv::Mat(), cv::Mat(), params);
-    SVM.save(full_path_svm.c_str());
-
-    // ==================================== SVM ================================
-
-    // ==================================== Boost ================================
-
-    CvBoost boost;
-    
-    boost.train(training_data_all, CV_ROW_SAMPLE, label_data_all);
-
-    std::string full_path_boost = classifier_path +"Boost_Model.xml";
-    boost.save(full_path_boost.c_str());
-
-    // ==================================== Boost ================================
-
-    // ==================================== Random Trees ================================
+        DataStruct testingData = TrainClassifier::buildTestingData();
 
 
-    // ==================================== Random Trees ================================
+        cv::Mat training_data = trainingData.features;
+        cv::Mat labels = trainingData.labels;
 
-
-    std::cout<<"Training completed"<<std::endl;
-
-    cv::Mat testSample = training_data_all.row(21);
-
-     float responseSVM = SVM.predict(testSample);
-     float responseBoost = boost.predict(testSample);
-
-     std::cout<<"===============================================" << std::endl;
-
-     std::cout<<"response SVM:" << responseSVM<<std::endl;
-     std::cout<<"response SVM:" << responseBoost<<std::endl;
-
-     std::cout<<"===============================================" << std::endl;
+        TrainClassifier::trainAllClassifier(training_data,labels,trainSVM,trainRF,trainAB);
 
     return 0;
 }
